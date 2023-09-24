eadef9b30eae59ea00933b0672945371a374397e
Gizem <139917291+gizemtmsv@users.noreply.github.com>
2023-09-24 18:22:14 +0300

    deneme

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -7,7 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the roo
+  //t of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
